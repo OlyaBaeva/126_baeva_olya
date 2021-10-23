8c9e5906f3beeb3413446ddedc5f3cd8534cb380
BaevaOlya <olyabaewa2014@mail.ru>
2021-10-23 18:57:06 +0300

    Удалить

---
+++ bonus/Crossword/src/Crossword.java
@@ -0,0 +1,22 @@
+public class Crossword {
+public static void main (String args[]){
+char z;
+char R201,R200,R204,R203,R206,R202,R185,R187,R188;
+R201='\u2554';
+R200='\u2550';
+R203='\u2550';
+R204='\u2560';
+R206='\u256c';
+R202=202;
+R185='\u2563';
+R187='\u2557';
+R188='\u255d';
+//for (int i=1; i<=120; i++) System.out.print(i);
+    z='X';
+    System.out.println("   "+ z+"  " + z + "  " + z + z + z +"    " + z+ z+ "    "+ z + z +"    "+z+z+"   "+z+z+z+
+            "    "+z+z+"   "+z+z+z+"     "+z +z);
+    System.out.println("   "+R201+R203+R187);
+    System.out.println("   "+R204+R206+R185);
+    System.out.println("   "+R200+R202+R188);
+}
+}
