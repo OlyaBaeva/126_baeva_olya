f9ce4fcbea71ea55d4d09726d00e0228931f3aef
BaevaOlya <olyabaewa2014@mail.ru>
2022-01-10 18:15:20 +0300

    add addContract_addContractWithNumberAndDate_ContractsCountEqualsOne

--- hw/1sem/task_10/src/Payment_system_code/PaymentSystem.java
+++ hw/1sem/task_10/src/Payment_system_code/PaymentSystem.java
@@ -4,11 +4,20 @@ import java.util.*;
 
 public class PaymentSystem {
 
+    private int contractsCount;
+    private PaymentSystem(){
+        contractsCount = 0;
+    }
+
+    public void addContract(String name, String phone) {
+        contractsCount++;
+    }
+
     public static PaymentSystem create(){
         return new PaymentSystem();
     }
 
     public int getContractsCount(){
-        return 0;
+        return contractsCount;
     }
 }
--- hw/1sem/task_10/src/Payment_system_test/FirstTest.java
+++ hw/1sem/task_10/src/Payment_system_test/FirstTest.java
@@ -4,9 +4,17 @@ import Payment_system_code.*;
 import org.junit.*;
 
 public class FirstTest extends Assert {
+
     @Test
     public void create_EmptyPaymentSystem_ContractsCountEqualsZero(){
         PaymentSystem paymentSystem = PaymentSystem.create();
         assertEquals(0, paymentSystem.getContractsCount());
+    }
+
+    @Test
+    public void addContract_addContractWithNumberAndDate_ContractsCountEqualsOne(){
+        PaymentSystem paymentSystem = PaymentSystem.create();
+        paymentSystem.addContract("number", "date");
+        assertEquals(1, paymentSystem.getContractsCount());
     }
 }
