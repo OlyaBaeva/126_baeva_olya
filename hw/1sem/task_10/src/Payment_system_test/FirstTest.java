2a4b23697ccbb18f4ee668cbb74d2f55a15b6e1e
BaevaOlya <olyabaewa2014@mail.ru>
2022-01-10 17:34:55 +0300

    Создан тест create_EmptyPaymentSystem_ContractsCountEqualsZero

--- hw/1sem/task_10/src/Payment_system_test/FirstTest.java
+++ hw/1sem/task_10/src/Payment_system_test/FirstTest.java
@@ -3,4 +3,9 @@ package Payment_system_test;
 import org.junit.*;
 
 public class FirstTest extends Assert {
+    @Test
+    public void create_EmptyPaymentSystem_ContractsCountEqualsZero(){
+        PaymentSystem paymentSystem = PaymentSystem.create();
+        assertEquals(0, paymentSystem.getContractsCount());
+    }
 }
